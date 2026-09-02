92f8313421a0e1217bc97890c9ef7916e690cf3f
Chaitanya Shekhar Pandey <113713770+Chaitanya-Shekhar-Pandey@users.noreply.github.com>
2026-09-02 12:25:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,23 @@ from datetime import datetime
 from zoneinfo import ZoneInfo
 from prometheus_client import Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
 import time
+import os
+
+class PrefixMiddleware:
+    def __init__(self, app, prefix=""):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+        if environ["PATH_INFO"].startswith(self.prefix):
+            environ["PATH_INFO"] = environ["PATH_INFO"][len(self.prefix):]
+            environ["SCRIPT_NAME"] = self.prefix
+            return self.app(environ, start_response)
+        else:
+            start_response("404", [("Content-Type", "text/plain")])
+            return [b"This URL does not belong to the app."]
+
+app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix=os.environ.get("URL_PREFIX", ""))
 
 app = Flask(__name__)
 IST = ZoneInfo("Asia/Kolkata")
